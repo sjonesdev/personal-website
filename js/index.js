c26f5c4c9848f9c460bcca725b861199de2b14a2
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2021-10-19 12:50:16 -0500

    Fixed contact form error messages

--- js/index.js
+++ js/index.js
@@ -4,13 +4,26 @@ const emailInput = contactForm.querySelector('input[type="email"]');
 const msgInput = contactForm.querySelector('textarea');
 const inputs = [nameInput, emailInput, msgInput];
 
+const nameErr = contactForm.querySelector('p.name-err');
+const emailErr = contactForm.querySelector('p.email-err');
+const msgErr = contactForm.querySelector('p.msg-err');
+const errs = [nameErr, emailErr, msgErr];
+
 for(let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('focus', e => {
         e.target.classList.add('input--active');
     });
 
+    inputs[i].addEventListener('input', e => {
+        if(e.target.value) {
+            errs[i].classList.add('hidden');
+        }
+    });
+
     inputs[i].addEventListener('blur', e => {
-        if(!e.target.value)
+        if(!e.target.value) {
             e.target.classList.remove('input--active');
+            errs[i].classList.remove('hidden');
+        }
     });
 }
