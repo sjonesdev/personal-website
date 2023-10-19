567f60f45da6825a7244774e2ddbbf25bb29b39c
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2023-10-19 10:11:18 -0700

    delete resume page

--- scripts/resume.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    const fadeTarget = document.querySelector('.loader-wrapper');
-    fadeTarget.classList.add('fade-out');
-    setTimeout(() => {fadeTarget.classList.add('none')}, 500);
-};
